88a3164a5edb9c47c1286e3d536fdb6a4a4542b2
Sylvester <hello@slvstr.dev>
2023-07-24 21:33:21 +0200

    Improve Button styling

--- src/components/ui/Button/Button.stories.ts
+++ src/components/ui/Button/Button.stories.ts
@@ -7,7 +7,6 @@ const meta = {
   component: Button,
   args: {
     children: 'Button',
-    onClick: () => {},
   },
   parameters: {
     layout: 'centered',
@@ -18,3 +17,15 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    isDisabled: true,
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    isLoading: true,
+  },
+};
