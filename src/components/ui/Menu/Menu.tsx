0033880656027920b26d9c54e41f06f5c4cf3851
Sylvester <hello@slvstr.dev>
2023-08-02 21:07:56 +0200

    Add optional classname to Icon & Menu

--- src/components/ui/Menu/Menu.tsx
+++ src/components/ui/Menu/Menu.tsx
@@ -1,15 +1,22 @@
 'use client';
 
+import { PropsWithChildren } from 'react';
+
 import { useTranslations } from 'next-intl';
 import { usePathname } from 'next-intl/client';
 import Link from 'next-intl/link';
+import { twMerge } from 'tailwind-merge';
 
-export default function Menu() {
+export interface MenuProps extends PropsWithChildren {
+  className?: string;
+}
+
+export default function Menu({ className }: MenuProps) {
   const t = useTranslations('components.menu');
   const pathname = usePathname();
 
   return (
-    <nav>
+    <nav className={twMerge('', className)}>
       <ul className="flex gap-4">
         <li
           className={`${
