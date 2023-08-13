4fe9a3828db6c4461a514525a6a551486f583197
Sylvester <hello@slvstr.dev>
2023-08-13 16:40:06 +0200

    Adjust button to have better defaults

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -31,25 +31,24 @@ export function Button({
 }
 
 export const button = tv({
-  base: 'relative inline-flex cursor-pointer items-center justify-center border uppercase transition-opacity hover:opacity-50 active:opacity-75',
+  base: 'relative inline-flex cursor-pointer items-center justify-center uppercase transition-opacity hover:opacity-50 active:opacity-75',
   slots: {
     label: '',
     loader: 'absolute inset-0 animate-spin',
   },
   variants: {
     color: {
-      brand: 'border-theme-brand-200 bg-theme-brand-100 text-theme-brand-300',
+      brand: 'border border-theme-brand-200 bg-theme-brand-100 text-theme-brand-300',
       brandRing:
-        'border-theme-brand-200 bg-theme-brand-100 text-theme-brand-300 ring-4 ring-theme-brand-100',
-      muted: 'border-theme-muted bg-white',
-      mutedRing: 'border-theme-muted bg-white ring-4 ring-theme-muted',
+        'border border-theme-brand-200 bg-theme-brand-100 text-theme-brand-300 ring-4 ring-theme-brand-100',
+      muted: 'border border-theme-muted bg-white',
+      mutedRing: 'border border-theme-muted bg-white ring-4 ring-theme-muted',
     },
     size: {
       sm: 'px-2 text-sm',
       md: 'px-4 py-1 text-base',
       lg: 'px-6 py-2 text-lg',
     },
-
     isDisabled: {
       true: 'pointer-events-none opacity-25',
     },
@@ -63,6 +62,5 @@ export const button = tv({
   defaultVariants: {
     isDisabled: false,
     isLoading: false,
-    size: 'md',
   },
 });
