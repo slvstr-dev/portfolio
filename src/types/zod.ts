3ba7bdfd22b231d59a8909638e694373dbcca466
Sylvester <hello@slvstr.dev>
2023-08-16 18:31:38 +0200

    Trying to fix Zod build issues on Vercel

--- src/types/zod.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const contactFormSchema = z.object({
-  subject: z.string().optional(),
-  body: z.string().min(1, { message: 'Please enter a message' }),
-});
-
-export type ContactFormSchema = z.infer<typeof contactFormSchema>;
